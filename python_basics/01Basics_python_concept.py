dd1be86446d859b91f97ec6a1f5b7c280a8a79fa
anuj-kumar-74 <anujkumaryadav7473@gmail.com>
2024-11-16 20:50:51 +0530

    done with the basics of python

---
+++ python_basics/01Basics_python_concept.py
@@ -0,0 +1,39 @@
+'''
+Name: Anuj kumar
+Aim: Python revision
+Date: 16th Nov 2024
+
+'''
+
+'''
+- Index
+
+'''
+# Mathematical operations
+print(200+20) # add
+print(200-2) # sub
+print(200*2) # multiply
+print(200/2) # division
+print(200%3) # modulus
+print(2-3*(200+3)) #complex operation
+print(20.0*3) # float
+# print(200/0) # 0 division error
+print(200//3) # int division
+print(2**3) # 2 power of 3 = 8
+print(4**(1/2)) # sqrt of 4
+
+# String in python
+print('hello world')
+print("He\'s a good boy.")
+
+# User input
+firstname = input('Enter your first name: ')
+lastname = input('Enter your last name: ')
+print('Your full name is ', firstname + ' ' + lastname)
+
+# String operations
+print(type(8), type('8'))
+print('hello'+' world')
+
+# Data types in python
+# Naming python variables
